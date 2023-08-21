cf0850e63e716d6c7a62c6c75f30a2102228aeb6
Félix <Felix.sotelo.q@outlook.es>
2023-08-21 17:45:35 -0400

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Felix Sotelo on 21-08-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
